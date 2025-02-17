ba1794ab77c017028b50d4a279238525e419ff32
Ino-Mier <mansurpoke951@gmail.com>
2025-02-17 11:48:01 +0500

    Update task

--- JSON/lab4_pp2_json.py
+++ JSON/lab4_pp2_json.py
@@ -1,32 +1,18 @@
 import json
-from datetime import datetime
 
-with open("sample-data.json", "r", encoding="utf-8") as f:
+with open("simple-data.json", "r", encoding="utf-8-sig") as f:
     data = json.load(f)
 
-target_days = {0, 2}
-filtered_interfaces = {"Mon, Wed": [], "Other Days": []}
-
+interfaces = []
 for item in data["imdata"]:
     attributes = item["l1PhysIf"]["attributes"]
-    mod_ts = attributes.get("modTs", "")
+    interfaces.append((attributes["dn"], attributes.get("speed", "N/A"), attributes.get("mtu", "N/A")))
 
-    try:
-        date_obj = datetime.strptime(mod_ts, "%Y-%m-%dT%H:%M:%S.%f%z")
-    except ValueError:
-        continue
-
-    category = "Mon, Wed" if date_obj.weekday() in target_days else "Other Days"
-    filtered_interfaces[category].append(attributes)
-
-def print_table(title, interfaces):
-    print(f"\n{title}:")
-    print("=" * 80)
-    print(f"{'DN':<50} {'Description':<20} {'Speed':<10} {'MTU'}")
-    print("-" * 80)
-
-    for interface in interfaces:
-        print(f"{interface['dn']:<50} {interface.get('descr', 'N/A'):<20} {interface['speed']:<10} {interface['mtu']}")
-
-print_table("Interfaces (Mon, Wed)", filtered_interfaces["Mon, Wed"])
-print_table("Interfaces (Other Days)", filtered_interfaces["Other Days"])c
+print("Interface Status")
+print("=" * 50)
+print(f"{'DN':<47} {'Speed':<7} {'MTU'}")
+print("-" * 50)
+for dn, speed, mtu in interfaces:
+    print(f"{dn:<47} {speed:<7} {mtu}")
+    print("=" * 50)
+    print(f"Total Interfaces: {len(interfaces)}")
